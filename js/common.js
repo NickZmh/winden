f9b7251ad153f6afef64e5b512d46251129c8f18
NickZmh <nickzmh92@gmail.com>
2021-08-03 16:21:22 +0300

    improve another sections

--- js/common.js
+++ js/common.js
@@ -39,5 +39,37 @@ $(function() {
 	wow.init();
 
 
+	$('.paroller').paroller({
+		direction: 'horizontal',
+		transition: 'transform 0.3s ease-out'
+	});
+	$('.paroller-upper').paroller({
+		direction: 'horizontal',
+		transition: 'transform 0.3s ease-out'
+	});
+
+
+	function initAcc(elem, option){
+		document.addEventListener('click', function (e) {
+			if (!e.target.matches(elem+' .a-btn')) return;
+			else{
+				if(!e.target.parentElement.classList.contains('active')){
+					if(option==true){
+						var elementList = document.querySelectorAll(elem+' .a-container');
+						Array.prototype.forEach.call(elementList, function (e) {
+							e.classList.remove('active');
+						});
+					}
+					e.target.parentElement.classList.add('active');
+				}else{
+					e.target.parentElement.classList.remove('active');
+				}
+			}
+		});
+	}
+
+	initAcc('.accordion.v1', true);
+
+
 
 });
